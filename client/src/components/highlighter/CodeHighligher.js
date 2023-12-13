91e40c5ec799b9518b439898c48b338ba5ed168a
Tal Cohen <talcohen318@gmail.com>
2023-12-13 15:19:51 +0200

    fix

--- client/src/components/highlighter/CodeHighligher.js
+++ client/src/components/highlighter/CodeHighligher.js
@@ -3,10 +3,10 @@ import React, {useEffect, useRef, useState} from 'react';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/androidstudio.css';
 
-const CodeHighlighter = ({language = 'javascript', initialCode, value, onChange, disabled}) => {
+const CodeHighlighter = ({language = 'javascript', initialCode, value,  onChange, disabled}) => {
     const codeRef = useRef(null);
     const [code, setCode] = useState(initialCode);
-
+    const [textCode, setTextCode] = useState("");
     useEffect(() => {
         if (codeRef.current) {
             hljs.highlightBlock(codeRef.current);
@@ -16,6 +16,7 @@ const CodeHighlighter = ({language = 'javascript', initialCode, value, onChange,
     const handleCodeChange = (e) => {
         const newText = e.target.value;
         setCode(newText);
+        setTextCode(newText);
         onChange(newText);
     };
 
@@ -23,7 +24,7 @@ const CodeHighlighter = ({language = 'javascript', initialCode, value, onChange,
 
     return (
         <div>
-            <textarea readOnly={disabled} value={value} onChange={handleCodeChange} rows={10} style={{width: '100%'}}/>
+            <textarea  value={textCode} onChange={handleCodeChange} rows={10} style={{width: '100%'}}/>
             <pre>
         <code ref={codeRef} dangerouslySetInnerHTML={{__html: highlightedCode}}/>
       </pre>
